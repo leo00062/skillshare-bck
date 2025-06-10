064ad4c8da4ca703be810d9633ec74f837d17947
leo00062 <carpentierleo2001@gmail.com>
2025-06-10 16:11:46 +0200

    Update

--- database/init-skillshare.sql
+++ database/init-skillshare.sql
@@ -9,6 +9,7 @@ CREATE TABLE `user` (
     avatar VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
+    `role`
     created_at DATETIME  
    
 );
